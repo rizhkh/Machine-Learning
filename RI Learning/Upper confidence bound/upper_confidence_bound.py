70ad4ee01576b8a8f465502809275aa8a7060b54
Riz <mrk_rizwan@hotmail.com>
2020-04-08 19:31:43 -0400

    completed ucb algoorithm

--- RI Learning/Upper confidence bound/upper_confidence_bound.py
+++ RI Learning/Upper confidence bound/upper_confidence_bound.py
@@ -17,65 +17,32 @@ number_of_selections = [0] * d # number_of_selections is known as Ni(n)
 sum_of_rewards = [0] * d # sum_of_rewards is known as Ri(n)
 
 ads_selected = [] #Step 3: ads_selected is from step 3
+total_reward = 0 # Step 3 from algorithm description
 
 # Step 2:
 N=10000
 for n in range(0,N): # this for look is for the 10k indexes
-        
+        ad = 0 # Step 3 - this is the index of ADS from dataset
         max_upper_bound = 0 # Step 3
-        
-        ad = 0 # Step 3
-        
         for i in range(0,d): # this for loop is for the 10 ads that exist
-                
-                average_reward = sum_of_rewards[i]/number_of_selections[i] # average_reward is ri(n)
-                
-                delta_i = math.sqrt ( 3/2 *  math.log(n+1)/number_of_selections[i]) # delta_i is delta symbol Δ(n) = sqrt( 3/2 * log(n)/number_of_selections[i] )
-                # n+1 is because the data starts from 1 and here n was 0 so thats why 1 was added
-                
-                upper_bound = average_reward + average_reward  # upper bound is ri(n)+Δ(n)
-                
+                if( number_of_selections[i] > 0): # this means if ad version of i was selected once this condition is used
+                        average_reward = sum_of_rewards[i]/number_of_selections[i] # average_reward is ri(n)
+                        delta_i = math.sqrt ( 3/2 *  math.log(n+1) / number_of_selections[i]) # delta_i is delta symbol Δ(n) = sqrt( 3/2 * log(n)/number_of_selections[i] )
+                        # n+1 is because the data starts from 1 and here n was 0 so thats why 1 was added
+                        upper_bound = average_reward + delta_i  # upper bound is ri(n)+Δ(n)
+                else:
+                     upper_bound = 1e400 # 10 to the power of 400 -> 10^400
                 if upper_bound > max_upper_bound: # Step 3
-                        
                         max_upper_bound = upper_bound # Step 3
                         ad = i
-        
+        ads_selected.append(ad) # Step 3
+        number_of_selections[ad] = number_of_selections[ad]  + 1
+        reward = dataset.values[ n, ad ] # n is row from dataset and ad is the column from dataset
+        sum_of_rewards[ad] = sum_of_rewards[ad] + reward
+        total_reward = total_reward + reward
 
-
-
-
-
-#
-## Implementing UCB
-#import math
-#N = 10000
-#d = 10
-#ads_selected = []
-#numbers_of_selections = [0] * d
-#sums_of_rewards = [0] * d
-#total_reward = 0
-#for n in range(0, N):
-#    ad = 0
-#    max_upper_bound = 0
-#    for i in range(0, d):
-#        if (numbers_of_selections[i] > 0):
-#            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
-#            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
-#            upper_bound = average_reward + delta_i
-#        else:
-#            upper_bound = 1e400
-#        if upper_bound > max_upper_bound:
-#            max_upper_bound = upper_bound
-#            ad = i
-#    ads_selected.append(ad)
-#    numbers_of_selections[ad] = numbers_of_selections[ad] + 1
-#    reward = dataset.values[n, ad]
-#    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
-#    total_reward = total_reward + reward
-#
-## Visualising the results
-#plt.hist(ads_selected)
-#plt.title('Histogram of ads selections')
-#plt.xlabel('Ads')
-#plt.ylabel('Number of times each ad was selected')
-#plt.show()
+plt.hist(ads_selected)
+plt.title('Ads Selections')
+plt.xlabel('Ads')
+plt.ylabel('# of times Ad was selected')
+plt.show()
