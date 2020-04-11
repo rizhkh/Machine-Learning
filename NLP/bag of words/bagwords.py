ad721d5b6246b588dd0ae0d44672b00e32830f57
Riz <mrk_rizwan@hotmail.com>
2020-04-11 18:57:13 -0400

    bag of words code

--- NLP/bag of words/bagwords.py
+++ NLP/bag of words/bagwords.py
@@ -51,5 +51,18 @@ sparse_matrix_X = count_vectorizer.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values # y is the depenedent variable to train this ML model - Y is basically the 1 0 of likes and dislikes from dataset
 
 
+## Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(sparse_matrix_X , y, test_size = 0.20, random_state = 0)
 
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
 
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
