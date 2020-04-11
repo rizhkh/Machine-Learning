8d5319c607c6f13ab2c45012fc2662721031e5b6
Riz <mrk_rizwan@hotmail.com>
2020-04-11 15:09:31 -0400

    bag of words code

--- NLP/bag of words/bagwords.py
+++ NLP/bag of words/bagwords.py
@@ -38,47 +38,18 @@ for i in range(0, len(dataset)):
         review = ' '.join(review) # converted the list back to string
         corpus.append(review)
 
+
 # Bag of words model
 
+from sklearn.feature_extraction.text import CountVectorizer
+## here we proceed to tokenization for the words we create
+# count_vectorizer = CountVectorizer()
+count_vectorizer = CountVectorizer(max_features = 1500) # adding max_featurse to filter out the words
+# 1500 is taken from sparse_matrix_x size if you run# count_vectorizer = CountVectorizer() without params (if you explore it it will show size of 1000,1565)
+#creating a sparse matrix - sparse matrix is nothing but features (matrix for features)
+sparse_matrix_X = count_vectorizer.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values # y is the depenedent variable to train this ML model - Y is basically the 1 0 of likes and dislikes from dataset
 
-#
-## Importing the dataset
-#dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
-#
-## Cleaning the texts
-#import re
-#import nltk
-#nltk.download('stopwords')
-#from nltk.corpus import stopwords
-#from nltk.stem.porter import PorterStemmer
-#corpus = []
-#for i in range(0, 1000):
-#    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
-#    review = review.lower()
-#    review = review.split()
-#    ps = PorterStemmer()
-#    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-#    review = ' '.join(review)
-#    corpus.append(review)
-#
-## Creating the Bag of Words model
-#from sklearn.feature_extraction.text import CountVectorizer
-#cv = CountVectorizer(max_features = 1500)
-#X = cv.fit_transform(corpus).toarray()
-#y = dataset.iloc[:, 1].values
-#
-## Splitting the dataset into the Training set and Test set
-#from sklearn.cross_validation import train_test_split
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
-#
-## Fitting Naive Bayes to the Training set
-#from sklearn.naive_bayes import GaussianNB
-#classifier = GaussianNB()
-#classifier.fit(X_train, y_train)
-#
-## Predicting the Test set results
-#y_pred = classifier.predict(X_test)
-#
-## Making the Confusion Matrix
-#from sklearn.metrics import confusion_matrix
-#cm = confusion_matrix(y_test, y_pred)
+
+
+
